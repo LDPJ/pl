bc1ad087058b3fc7844a2d13e8a6a45d8d6b36cc
Nah.Myself( ) <larrypaulingjr@gmail.com>
2017-08-31 14:41:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+var wavesurfer = WaveSurfer.create({
+  container: '#waveform',
+  waveColor: 'red',
+  progressColor: 'purple',
+});
+
+wavesurfer.play();
+
+
+wavesurfer.load(' pl/01 - Smoking Makes Me Happy(Prod. by Kwafi).mp3 ');
