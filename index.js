9ebbe40c1b90645211225f6fb3a31dea3a98665a
Nah.Myself( ) <larrypaulingjr@gmail.com>
2017-08-31 14:43:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ var wavesurfer = WaveSurfer.create({
 
 wavesurfer.play();
 
-
+wavesurfer.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
 wavesurfer.load(' pl/01 - Smoking Makes Me Happy(Prod. by Kwafi).mp3 ');
